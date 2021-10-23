742c35206b284b959bb21da8f1698557c132ed7b
Taylor Sutton <tsutton125+github@gmail.com>
2021-10-23 14:52:00 -0700

    Add the VM stack and arithmetic operations.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,21 @@ use rslox::vm::{self, *};
 
 fn main() {
     let mut chunk = Chunk::new();
-    let idx = chunk.add_constant(Value::Number(1.0));
+    let idx = chunk.add_constant(Value::Number(1.2));
     chunk.write_instruction(Instruction::Constant(idx), 1);
-    chunk.write_instruction(Instruction::Return, 2);
-    println!("{}", chunk.disassemble("test"));
+
+    let idx = chunk.add_constant(Value::Number(3.4));
+    chunk.write_instruction(Instruction::Constant(idx), 2);
+
+    chunk.write_instruction(Instruction::Add, 3);
+
+    let idx = chunk.add_constant(Value::Number(5.6));
+    chunk.write_instruction(Instruction::Constant(idx), 2);
+
+    chunk.write_instruction(Instruction::Divide, 2);
+
+    chunk.write_instruction(Instruction::Negate, 2);
+    chunk.write_instruction(Instruction::Return, 3);
     let mut machine = vm::Vm::new(chunk);
     machine.interpret().unwrap();
 }
--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -3,6 +3,18 @@ use std::{
     fmt::{Display, Write},
 };
 
+macro_rules! binary_arithmetic {
+    ($self:ident, $op:tt) => {
+	{
+            let b = $self.stack_pop()?;
+	    let Value::Number(b) = b;
+            let a = $self.stack_pop()?;
+	    let Value::Number(a) = a;
+	    $self.stack_push(Value::Number(a $op b))
+	}
+    };
+}
+
 /// A single instruction, in a parsed/type-safe format.
 /// This type is helpful as an intermediate representation while compiling, before serializing into bytes.
 /// For execution, we have two options:
@@ -12,14 +24,29 @@ use std::{
 /// I'm starting off with choice 1, on the "don't prematurely optimize" guideline, but we'll see
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Instruction {
-    /// Return the control flow from the enclosing function.
+    /// Pop and print the top value
     Return,
     /// Load a constant by its index into the constant table.
     Constant(u8),
+    /// Negate the top value on the stack
+    Negate,
+    /// If stack is TOP: b, a ..., pop two and push (a+b)
+    Add,
+    /// If stack is TOP: b, a ..., pop two and push (a-b)
+    Subtract,
+    /// If stack is TOP: b, a ..., pop two and push (a*b)
+    Multiply,
+    /// If stack is TOP: b, a ..., pop two and push (a/b)
+    Divide,
 }
 
 const RETURN_OP_CODE: u8 = 0;
 const CONSTANT_OP_CODE: u8 = 1;
+const NEGATE_OP_CODE: u8 = 2;
+const ADD_OP_CODE: u8 = 3;
+const SUBTRACT_OP_CODE: u8 = 4;
+const MULTIPLY_OP_CODE: u8 = 5;
+const DIVIDE_OP_CODE: u8 = 6;
 
 impl Instruction {
     /// Try to parse an instruction from the beginning of some bytes, returning the number of bytes that the instruction consists of
@@ -44,14 +71,24 @@ impl Instruction {
         match self {
             Self::Return => writer.write(&[RETURN_OP_CODE]),
             Self::Constant(u) => writer.write(&[CONSTANT_OP_CODE, *u]),
+            Self::Negate => writer.write(&[NEGATE_OP_CODE]),
+            Self::Add => writer.write(&[ADD_OP_CODE]),
+            Self::Subtract => writer.write(&[SUBTRACT_OP_CODE]),
+            Self::Multiply => writer.write(&[MULTIPLY_OP_CODE]),
+            Self::Divide => writer.write(&[DIVIDE_OP_CODE]),
         }
     }
 
     /// Number of bytes in the byte represention of this instruction
     pub fn num_bytes(&self) -> usize {
         match self {
-            Self::Return => 1,
-            Self::Constant(_) => 2,
+            Instruction::Return => 1,
+            Instruction::Constant(_) => 2,
+            Instruction::Negate => 1,
+            Instruction::Add => 1,
+            Instruction::Subtract => 1,
+            Instruction::Multiply => 1,
+            Instruction::Divide => 1,
         }
     }
 }
@@ -69,6 +106,11 @@ impl Display for Instruction {
         match self {
             Instruction::Return => write!(f, "OP_RETURN"),
             Instruction::Constant(u) => write!(f, "OP_CONSTANT {:4}", u),
+            Instruction::Negate => write!(f, "OP_NEGATE"),
+            Instruction::Add => write!(f, "OP_ADD"),
+            Instruction::Subtract => write!(f, "OP_SUBTRACT"),
+            Instruction::Multiply => write!(f, "OP_MULTIPLY"),
+            Instruction::Divide => write!(f, "OP_DIVIDE"),
         }
     }
 }
@@ -109,28 +151,34 @@ impl Chunk {
         idx
     }
 
+    fn get_constant(&self, idx: u8) -> &Value {
+        &self.constants[usize::from(idx)] // TODO can panic
+    }
+
+    fn disassemble_instruction(&self, instruction: &Instruction, ip: usize) -> String {
+        let line = self.lines[ip];
+        let mut ret = format!("i{:04} {:04} {}", ip, line, instruction);
+        #[allow(clippy::single_match)]
+        match instruction {
+            Instruction::Constant(u) => {
+                write!(
+                    &mut ret,
+                    " '{}'",
+                    self.constants[usize::try_from(*u).unwrap()]
+                )
+                .expect("writing to string");
+            }
+            _ => {}
+        }
+        ret
+    }
+
     /// Return a human-readable string for a chunk.
     pub fn disassemble(&self, title: &str) -> String {
         let mut ret = format!("== {} ==\n", title);
-        let mut offset = 0;
         for (i, instruction) in self.code.iter().enumerate() {
-            let line = self.lines[i];
-            write!(&mut ret, "{:04} {:04} {}", offset, line, instruction)
-                .expect("writing to string");
-            #[allow(clippy::single_match)]
-            match instruction {
-                Instruction::Constant(u) => {
-                    write!(
-                        &mut ret,
-                        " '{}'",
-                        self.constants[usize::try_from(*u).unwrap()]
-                    )
-                    .expect("writing to string");
-                }
-                _ => {}
-            }
+            ret.push_str(&self.disassemble_instruction(instruction, i));
             ret.push('\n');
-            offset += instruction.num_bytes();
         }
         ret
     }
@@ -157,11 +205,20 @@ impl Display for Value {
     }
 }
 
+const STACK_SIZE: usize = 256;
+
 /// A Vm is a stateful executor of chunks.
 #[derive(Debug, Clone)]
 pub struct Vm {
     chunk: Chunk,
     ip: usize,
+    // The book uses an array for the stack, which means that the values are stored in-line with the VM,
+    // rather than behind a pointer indirection.
+    // We have a few options to do that in Rust:
+    // - Use an array, but with Option or MaybeUninit to initialize the array, and track the valid
+    //   indices on the stack, same as the book.
+    // - Use a Vec; now accessing the stack requires goes out-of-line with the VM, but it's simpler
+    stack: Vec<Value>,
 }
 
 /// Errors that can be returned by running the intepreter.
@@ -177,11 +234,66 @@ pub enum InterpretError {
 impl Vm {
     /// The VM must be initialized with some code to run.
     pub fn new(chunk: Chunk) -> Self {
-        Vm { chunk, ip: 0 }
+        Vm {
+            chunk,
+            ip: 0,
+            stack: Vec::with_capacity(STACK_SIZE),
+        }
     }
 
     /// Run the interpreter until code finishes executing or an error occurs.
     pub fn interpret(&mut self) -> Result<(), InterpretError> {
-        todo!()
+        while self.ip < self.chunk.code.len() {
+            let instr = self.chunk.code[self.ip];
+            #[cfg(feature = "trace")]
+            {
+                print!("[ ");
+                for val in &self.stack {
+                    print!("{} ", val)
+                }
+                println!("]");
+                println!("{}", self.chunk.disassemble_instruction(&instr, self.ip));
+            }
+            self.execute(&instr)?;
+            self.ip += 1;
+        }
+        Ok(())
+    }
+
+    fn execute(&mut self, instruction: &Instruction) -> Result<(), InterpretError> {
+        match instruction {
+            Instruction::Return => {
+                let val = self.stack_pop()?;
+                println!("{}", val);
+                Ok(())
+            }
+            Instruction::Constant(idx) => self.stack_push(self.chunk.get_constant(*idx).clone()),
+            Instruction::Negate => {
+                let value = self.stack_pop()?;
+                #[allow(irrefutable_let_patterns)]
+                if let Value::Number(number) = value {
+                    self.stack_push(Value::Number(-number))
+                } else {
+                    Err(InterpretError::RuntimeError)
+                }
+            }
+            Instruction::Add => binary_arithmetic!(self, +),
+            Instruction::Subtract => binary_arithmetic!(self, -),
+            Instruction::Multiply => binary_arithmetic!(self, *),
+            Instruction::Divide => binary_arithmetic!(self, /),
+        }
+    }
+
+    fn stack_push(&mut self, value: Value) -> Result<(), InterpretError> {
+        if self.stack.len() >= STACK_SIZE {
+            Err(InterpretError::RuntimeError)
+        } else {
+            self.stack.push(value);
+            Ok(())
+        }
+    }
+
+    fn stack_pop(&mut self) -> Result<Value, InterpretError> {
+        self.stack.pop().ok_or(InterpretError::RuntimeError)
     }
 }
